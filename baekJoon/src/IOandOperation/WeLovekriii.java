fc12c1d2f0bb4af1a70e933f0b049d9205a07bee
SungCheon Ju <jution113@gmail.com>
2022-10-13 23:05:57 +0900

    221013 1000-APlusB and rename 10718 WeLoveKriii

--- baekJoon/src/IOandOperation/WeLovekriii.java
+++ baekJoon/src/IOandOperation/WeLovekriii.java
@@ -1,6 +1,6 @@
 package IOandOperation;
 
-public class Welovekriii {
+public class WeLovekriii {
     public static void main(String[] args) {
         System.out.println("강한친구 대한육군");
         System.out.println("강한친구 대한육군");
