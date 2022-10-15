c5abf95d198304acc22e74c70ecba6f5735d901b
SungCheon Ju <jution113@gmail.com>
2022-10-15 14:20:39 +0900

    221015 10998-AMultiplyB, 1008-ADivideB

---
+++ baekJoon/src/IOandOperation/ADivideB.java
@@ -0,0 +1,12 @@
+package IOandOperation;
+
+import java.util.Scanner;
+
+public class ADivideB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(a/b);
+    }
+}
