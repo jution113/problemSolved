31ab2836b212ed1e32926fe8b3d5df005eedcf56
SungCheon Ju <jution113@gmail.com>
2022-10-17 00:11:28 +0900

    221017 10869-ArithmeticOperation 10926-AwesomeMark

---
+++ baekJoon/src/IOandOperation/AwesomeMark.java
@@ -0,0 +1,11 @@
+package IOandOperation;
+
+import java.util.Scanner;
+
+public class AwesomeMark {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.printf("%s??!", input);
+    }
+}
