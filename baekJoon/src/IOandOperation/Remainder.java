dd6ab0978c13684c9591466b18445179c083051e
SungCheon Ju <jution113@gmail.com>
2022-10-18 22:47:24 +0900

    221018 10430-Remainder

---
+++ baekJoon/src/IOandOperation/Remainder.java
@@ -0,0 +1,18 @@
+package IOandOperation;
+
+import java.util.Scanner;
+
+// 10430
+public class Remainder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println( (a+b)%c );
+        System.out.println( ((a%c) + (b%c))%c );
+        System.out.println( (a*b)%c );
+        System.out.println( ((a%c) * (b%c))%c );
+    }
+}
