d6604190676602e2abc80828972b8adb14aa873c
SungCheon Ju <jution113@gmail.com>
2022-10-22 23:16:41 +0900

    221022 10171-Cat

---
+++ baekJoon/src/IOandOperation/Cat.java
@@ -0,0 +1,11 @@
+package IOandOperation;
+
+// 10171
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
